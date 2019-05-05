dc6c03fca19cb6e3339df9aba412fcaa05d0a99a
xujiyao <455601971@qq.com>
2019-05-05 16:33:23 +0800

    commit my project

---
+++ src/main/java/com/xjy/SpringBootDemo/Controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.xjy.SpringBootDemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping("hello")
+    public String helloWorld(){
+        return "hello world";
+    }
+}
+//com.xjy.SpringBootDemo.Controller; 需要把这个包放到扫描器里，@AutoConfigurationPackage起的作用
