e7f97178b993df56bc5f76b62698ded1ef719c37
xujiyao <455601971@qq.com>
2019-05-05 17:01:39 +0800

    version 1.0 this is version 1.0

--- src/main/java/com/xjy/SpringBootDemo/Controller/HelloWorldController.java
+++ src/main/java/com/xjy/SpringBootDemo/Controller/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
     @ResponseBody
     @RequestMapping("hello")
     public String helloWorld(){
-        return "hello s3pr3ingboot !!!!";
+        return "hello spring version 1.0这是版本1.0";
     }
 }
 //com.xjy.SpringBootDemo.Controller; 需要把这个包放到扫描器里，@AutoConfigurationPackage起的作用
